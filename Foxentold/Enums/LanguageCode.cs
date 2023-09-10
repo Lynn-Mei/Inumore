cf786cb1a7267a489aeb4d57be945f50e0fbff91
Lynn-Mei <hugo.murura@gmail.com>
2023-09-10 14:27:42 +0200

    Original Ressources

---
+++ Foxentold/Enums/LanguageCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foxentold.Enums
+{
+    public enum LanguageCode
+    {
+        JP,
+        EN,
+        FR,
+        ID,
+        DE,
+        SV
+    }
+}
