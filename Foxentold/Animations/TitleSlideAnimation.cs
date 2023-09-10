f0cc22a8576fc3255ea0c032e0a788782093c24e
Lynn-Mei <hugo.murura@gmail.com>
2023-09-10 14:28:26 +0200

    First basic animation

---
+++ Foxentold/Animations/TitleSlideAnimation.cs
@@ -0,0 +1,26 @@
+﻿using Foxentold.Drawables;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foxentold.Animations
+{
+    public class TitleSlideAnimation : Animation
+    {
+        public void setFrames(List<GameItem> frames)
+        {
+            foreach(GameItem frame in frames)
+            {
+                this.AddFrame(frame);
+            }
+        }
+        protected override void DefineAnimation()
+        {
+            this.speed = 10;
+            this.movement.Add(new Microsoft.Xna.Framework.Vector2(200, 150));
+        }
+    }
+}
