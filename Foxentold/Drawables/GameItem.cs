3a97d7b2dde8d3383a83406722f2770d02a13344
Lynn-Mei <hugo.murura@gmail.com>
2023-09-10 14:26:59 +0200

    GameItem and Scene System v1

---
+++ Foxentold/Drawables/GameItem.cs
@@ -0,0 +1,65 @@
+﻿using Foxentold.Scenes;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foxentold.Drawables
+{
+    public abstract class GameItem:IDrawable
+    {
+        protected Scene parent;
+        protected int x=0;
+        protected int y=0;
+        protected int relativeX = 0;
+        protected int relativeY = 0;
+        public GameItem(Scene parent, int x, int y)
+        {
+            this.parent = parent;
+            this.x = x;
+            this.y = y;
+            this.updateRelativePos();
+        }
+
+        public abstract int DrawOrder { get; }
+
+        public abstract bool Visible { get; }
+
+        public abstract event EventHandler<EventArgs> DrawOrderChanged;
+        public abstract event EventHandler<EventArgs> VisibleChanged;
+
+        public abstract void Draw(GameTime gameTime);
+
+        public void applyVector(Vector2 vector)
+        {
+            Vector2 current = new Vector2(x, y);
+            current += vector;
+            this.x = (int)current.X;
+            this.y = (int)current.Y;
+            this.updateRelativePos();
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(x, y);
+            }
+
+            set
+            {
+                this.x = Convert.ToInt32(value.X);
+                this.y = Convert.ToInt32(value.Y);
+            }
+        }
+
+        public void updateRelativePos()
+        {
+            this.relativeX = this.parent.Coordinates.Item1 + this.x;
+            this.relativeY = this.parent.Coordinates.Item2 + this.y;
+        }
+
+    }
+}
---
+++ Foxentold/Scenes/Scene.cs
@@ -0,0 +1,61 @@
+﻿using Foxentold.Animations;
+using Foxentold.Drawables;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foxentold.Scenes
+{
+    public abstract class Scene
+    {
+        protected int x;
+        protected int y;
+        protected List<GameItem> drawable = new List<GameItem>();
+        protected List<GameItem> animatedDrawables = new List<GameItem>();
+        protected List<Animation> animations = new List<Animation>();
+
+        public (int, int) Coordinates { get { return (x, y); } }
+
+        public Scene(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            //Draw all drawables
+            foreach (GameItem drawable in drawable)
+            {
+                drawable.Draw(gameTime);
+            }
+            foreach (GameItem drawable in animatedDrawables)
+            {
+                drawable.Draw(gameTime);
+            }
+        }
+
+        protected abstract void AbstractedUpdate();
+
+        public void Update(GameTime gameTime)
+        {
+            animatedDrawables.Clear();
+            //Update the currently playing animations
+            foreach(Animation animation in animations)
+            {
+                if (animation.IsPlaying)
+                {
+                    animatedDrawables.Add(animation.Update(gameTime));
+                }
+                else
+                {
+                    animatedDrawables.Add(animation.GetStopedFrame());
+                }
+            }
+            this.AbstractedUpdate();
+        }
+    }
+}
