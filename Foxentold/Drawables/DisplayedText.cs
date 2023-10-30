1bca760342be3cedc4274118ee25b12ba4584db6
Lynn-Mei <hugo.murura@gmail.com>
2023-10-30 14:55:27 +0100

    Added new Color and Font Size attr to text

--- Foxentold/Drawables/DisplayedText.cs
+++ Foxentold/Drawables/DisplayedText.cs
@@ -15,6 +15,8 @@ namespace Foxentold.Drawables
     public class DisplayedText : GameItem
     {
         private string text;
+        private Color color;
+        private int fontSize;
 
         public override int DrawOrder { get { return 14; } }
 
@@ -23,16 +25,38 @@ namespace Foxentold.Drawables
         public override event EventHandler<EventArgs> DrawOrderChanged;
         public override event EventHandler<EventArgs> VisibleChanged;
 
-        public DisplayedText(Scene parent,int x, int y,string text) :base(parent,x,y)
+        public DisplayedText(Scene parent,int x, int y,string text, Color? color=null, int size=15) :base(parent,x,y)
         {
             this.text = text;
+            if(color == null) { 
+                this.color = Color.Black;
+            }
+            else
+            {
+                this.color = (Color)color;
+            }
 
+            this.fontSize = size;
         }
+
         public override void Draw(GameTime gameTime)
         {
             SpriteFont font = SpriteBatchManager.DefaultFont;
-            SpriteBatchManager.SpriteBatch.DrawString(font, this.text, new Vector2(relativeX, relativeY), Color.Black);
+            Vector2 position = new Vector2(relativeX, relativeY);
+            if (fontSize > 0)
+            {
+                float scale = fontSize / font.MeasureString(text).Y;
+                //position.Y -= (font.MeasureString(text).Y - font.MeasureString(text * scale).Y) / 2;
+
+                SpriteBatchManager.SpriteBatch.DrawString(font, text, position, color, 0f, Vector2.Zero, scale, SpriteEffects.None, 0);
+            }
+            else //if size is below 0 the text is rendered at default font size
+            {
+                SpriteBatchManager.SpriteBatch.DrawString(font, this.text, position, color);
+            }
+                
         }
+
 
     }
 }
