5db03abc40607f0d5d6ef22724f0ee9ef7727add
Lynn-Mei <hugo.murura@gmail.com>
2024-01-11 08:41:08 +0100

    Adding abstract Mob class

---
+++ Inumore/mobs/Mob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inumore.mobs
+{
+    public abstract class Mob
+    {
+        protected int x;
+        protected int y;
+        protected int HP;
+        protected int level;
+    }
+}
